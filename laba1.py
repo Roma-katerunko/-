80a66a831d1b999cadcc74b0ae41bdfb8c5ea2d5
Roma-katerunko <romakaterinko@gmail.com>
2025-09-03 23:29:19 +0300

    lada1

---
+++ laba1.py
@@ -0,0 +1,19 @@
+age=int(3+3)#додаваня
+
+billionth=float(3.4-1.2)#відніманя
+
+combat=str("Hello")
+
+doc=bool(True)
+
+english=[]
+
+doom=[1/2,"wozed",8*11]#ділен,множеня
+
+game={1//2,"Misha"}#обчислення неповної частки від ділення
+
+human=(1,2,3**4,34%48,1,7)#взведеня до степеня,обчисленя остачі
+
+zeus={1:"God"}
+
+print("age","int:",age,"billionth","float:",billionth,"combat","str:",combat,"doc","bool:",doc,"english","list:",english,"doom","list:",doom,"game","set:",game,"human","tuple:",human,"zeus","dict:",zeus)
